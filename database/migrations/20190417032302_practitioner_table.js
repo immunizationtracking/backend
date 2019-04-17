639b64f3fbf2b5ddaef73a301d77099c533df916
Sem8 <mssemmi8@gmail.com>
2019-04-17 03:27:07 -0400

    still debugging heroku deployment issues

---
+++ database/migrations/20190417032302_practitioner_table.js
@@ -0,0 +1,23 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("practitionerInfo", practitionerTbl => {
+      practitionerTbl.increments();
+      practitionerTbl.string("firstName").notNullable();
+      practitionerTbl.string("lastName").notNullable();
+      practitionerTbl.string("title");
+      practitionerTbl.string("nameOfOffice").notNullable();
+  
+      practitionerTbl
+        .integer("practitionerUserId")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('practitionerInfo');
+  };
+  
---
+++ database/migrations/20190417032538_vaccines_table.js
@@ -0,0 +1,36 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable("vaccines", vaccinesTbl => {
+      vaccinesTbl.increments();
+  
+      vaccinesTbl.string("immunizationName").notNullable();
+      vaccinesTbl.date("dateReceived").notNullable();
+      vaccinesTbl.text("placeReceived").notNullable();
+      vaccinesTbl.string("givenBy").notNullable();
+      vaccinesTbl.date("nextShotDue");
+      vaccinesTbl.text("doseInfo");
+      vaccinesTbl.text("doseNumber").notNullable();
+      vaccinesTbl.boolean("hasAccess");
+      vaccinesTbl
+        .integer("patientInfo_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("patientInfo")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+  
+      vaccinesTbl
+        .integer("practitioner_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("practitionerInfo")
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE");
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+      return knex.schema.dropTableIfExists('vaccines');
+  };
+  
